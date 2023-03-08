1a635a3779938c01b0fa3eb27ee56db7e28ad1ba
IssacGon <126217451+IssacGon@users.noreply.github.com>
2023-03-07 23:31:46 -0600

    Add files via upload

---
+++ ticketsys/about.php
@@ -0,0 +1,46 @@
+<?php
+    $title ="Sobre Mi | ";
+    include "head.php";
+    include "sidebar.php";  
+?>
+
+    <div class="right_col" role="main"> <!-- page content -->
+        <div class="">
+            <div class="page-title">
+            <br>
+                <div class="row">
+            	    <div class="col-md-6 col-md-offset-3">
+                        <h1>Acerca del autor</h1>
+                        <p>Hola, somos <strong>Issac Gonzalez, Eduardo Lopez y Edwin Laureano creadores de <a>ticketsys</a>.</p>
+
+                        <br><br>
+
+                        <h2>TICKETSYS - <small>Sistema de Tickets de Soporte</small></h2>
+                        <p>TICKETSYS es un sistema de Tickets de soporte de proposito general, codigo abierto y gratis.</p>
+
+                        <h1 style="text-align: center;">TICKETSYS</h1>
+                        <h3>Descripcion</h3>
+                        <p>Ticketsys es un sistema para gestionar tickets de soporte usando proyectos, categorias, prioridades y mas, ideal para todo tipo de negocios, desarrollo de proyectos, gestion personal, etc, el sistema esta desarrollado con PHP y MySQL.</p>
+                        <p>TICKETSYS es un sistema de tickets de soporte en el que puedes rellenar tu perfil, agregar ticket, proyectos y categorias, generar reportes y mucho mas.</p>
+
+                        
+                        <h3>Modulos</h3>
+                        <p>Definimos a grandes rasgos los modulos generales del sistema</p>
+                        <ul>
+                        <li><b>Usuarios</b>: Se puede agregar editar y eliminar usuarios.</li>
+                        <li><b>Tickets</b>: Cada usuario puede agregar un ticket.</li>
+                        <li><b>Proyectos</b>: Cada usuario puede agregar los proyectos que desee.</li>
+                        <li><b>Perfiles</b>: Los usuarios pueden rellenar su perfil</li>
+                        <li><b>Categorias</b>: Los usuarios agregar categorias.</li>
+                        <li><b>Reportes</b>: Los usuarios pueden generar reportes.</li>
+                        </ul>
+
+                        <br><br>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- /page content -->
+
+<?php include "footer.php" ?>
